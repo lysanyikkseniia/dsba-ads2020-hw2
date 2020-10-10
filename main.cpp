16df772b2c195ab74b6621ba7c9755c49b9d9135
lysanyikkseniia <64256327+lysanyikkseniia@users.noreply.github.com>
2020-10-10 19:59:37 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main()
     Graph energyNetwork = inputGraph();
     Graph mst;
     double mstSize = MSTPrim(energyNetwork, mst);
-    std::cout << mstSize;
+    std::cout << mstSize << std::endl;
     mst.printEdges();
     return 0;
 }
--- prim.cpp
+++ prim.cpp
@@ -2,9 +2,9 @@
 
 void Graph::printEdges()
 {
-    for (int i = 0; i < size(); i++)
+    for (int i = 0; i < _edges.size(); i++)
         std::cout << _edges[i].first.first << ' ' << _edges[i].first.second << ' ' <<
-                  _edges[i].second.first << ' ' << _edges[i].second.second;
+                  _edges[i].second.first << ' ' << _edges[i].second.second << std::endl;
 }
 
 Graph inputGraph()
@@ -23,10 +23,10 @@ Graph inputGraph()
 
 double Distance(coordinates setofCoord, double a, double b)
 {
-    int a1 = setofCoord[a].first;
-    int b1 = setofCoord[a].second;
-    int a2 = setofCoord[b].first;
-    int b2 = setofCoord[b].second;
+    double a1 = setofCoord[a].first;
+    double b1 = setofCoord[a].second;
+    double a2 = setofCoord[b].first;
+    double b2 = setofCoord[b].second;
     return sqrt((a2 - a1) * (a2 - a1) + (b2 - b1) * (b2 - b1));
 }
 
@@ -43,11 +43,9 @@ double MSTPrim(Graph g, Graph& mst)
 {
     // Graph mst;
     int n = g.size();
-    std::vector<double> min_dist(n + 1, INT_MAX);
-
-    min_dist[0] = 0;
+    std::vector<double> minDist(n + 1, INT_MAX);
+    minDist[0] = 0;
     std::vector<bool> visited(n + 1);
-
     double res = 0;
     for (int i = 0; i < n; i++)
     {
@@ -55,23 +53,30 @@ double MSTPrim(Graph g, Graph& mst)
         int index = -1;
         for (int j = 0; j < n; j++)
         {
-            if (!visited[j] && (!f || min_dist[j] < min_dist[index]))
+            if (!visited[j] && (!f || minDist[j] < minDist[index]))
             {
                 index = j;
                 f = true;
             }
         }
         visited[index] = true;
-        res+= min_dist[index];
-        // тут запомнить i j
-        mst.pushEdge(std::make_pair((std::make_pair(g[i].first, g[i].second)),
-                std::make_pair(g[index].first, g[index].second)));
-        // std::cout << g[i].first << ' ' << g[i].second << ' ' << g[index].first << ' ' << g[index].second  << std::endl;
-
+        res+= minDist[index];
+        int parent = 0;
+        double dist = INT_MAX;
         for (int j = 0; j < n; j++)
-            if (!visited[j] && min_dist[j] > Distance(g, index, j))
-                min_dist[j] = Distance(g, index, j);
+        {
+            if (j != index && (Distance(g, index, j) < dist) && visited[j])
+            {
+                dist = Distance(g, index, j);
+                parent = j;
+            }
+        }
+        if (parent != index)
+            mst.pushEdge(std::make_pair((std::make_pair(g[parent].first, g[parent].second)),
+                std::make_pair(g[index].first, g[index].second)));
+        for (int j = 0; j < n; j++)
+            if (!visited[j] && minDist[j] > Distance(g, index, j))
+                minDist[j] = Distance(g, index, j);
     }
-    // std::cout << res;
     return res;
 }
